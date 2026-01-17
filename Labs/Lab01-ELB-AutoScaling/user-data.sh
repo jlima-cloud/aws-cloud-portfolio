fb8c3ce168ff292b0bbb07dc416cf65adf76f990
JOSE LIMA <joes_marcos@hotmail.com>
2026-01-17 20:34:21 -0300

    Install PHP and configure permissions in user-data.sh
    Updated user-data.sh to install PHP and set permissions for ec2-user.

--- Labs/Lab01-ELB-AutoScaling/user-data.sh
+++ Labs/Lab01-ELB-AutoScaling/user-data.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 yum update -y
-yum install -y httpd
+yum install -y httpd php
 
 systemctl start httpd
 systemctl enable httpd
 
-echo "Apache OK - Lab funcionando" > /var/www/html/index.html
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
